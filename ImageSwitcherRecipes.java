ee4a567e23a01f218f8e92da55bf2f5f14cb7a9c
amdecker <amdecker9@gmail.com>
2016-07-26 09:05:39 -0400

    Add files via upload

---
+++ ImageSwitcherRecipes.java
@@ -0,0 +1,62 @@
+package com.example.feelthemeal.feelthemeal;
+
+/**
+ * Created by adder_000 on 7/26/2016.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.ViewSwitcher;
+
+
+public class ImageSwitcherRecipes extends Activity {
+
+    private static final String[] TEXTS = { "Image #1", "Image #2", "Image #3" };
+    private static final int[] IMAGES = { R.mipmap.avocadotoast, R.mipmap.bananabread,
+            R.mipmap.berrysmoothie };
+    private int mPosition = 0;
+    private TextSwitcher mTextSwitcher;
+    private ImageSwitcherRecipes mImageSwitcher;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.breakfast);
+
+        mTextSwitcher = (TextSwitcher) findViewById(R.id.textSwitcher);
+        mTextSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                TextView textView = new TextView(ImageSwitcherRecipes.this);
+                textView.setGravity(Gravity.CENTER);
+                return textView;
+            }
+        });
+
+        mTextSwitcher.setInAnimation(this, android.R.anim.fade_in);
+        mTextSwitcher.setOutAnimation(this, android.R.anim.fade_out);
+
+        mImageSwitcher = (ImageSwitcherRecipes) findViewById(R.id.imageSwitcher);
+        mImageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView imageView = new ImageView(MainActivity.this);
+                return imageView;
+            }
+        });
+        mImageSwitcher.setInAnimation(this, android.R.anim.slide_in_left);
+        mImageSwitcher.setOutAnimation(this, android.R.anim.slide_out_right);
+
+        onSwitch(null);
+    }
+
+    public void onSwitch(View view) {
+        mTextSwitcher.setText(TEXTS[mPosition]);
+        mImageSwitcher.setBackgroundResource(IMAGES[mPosition]);
+        mPosition = (mPosition + 1) % TEXTS.length;
+    }
